11ab889105e843b46b3437b201c7bc8d3b09b9fd
ch-und | NodeValidator <batuoc263@hotmail.com>
2023-04-20 18:47:48 +0700

    Submit pfb api

--- api/submit_pfb.php
+++ api/submit_pfb.php
@@ -3,9 +3,13 @@
 const __DEFAULT_GAS__ = 80000;
 const __DEFAULT_FEE__ = 2000;
 
-if (isset($_POST)) {
+if (isset($_POST['tx_data'])) {
     try {
         $curl = curl_init();
+        
+        $random_bytes = random_bytes(8);
+        $namespace_id = bin2hex($random_bytes);
+        $tx_data = bin2hex(utf8_encode($_POST['tx_data']));
 
         curl_setopt_array($curl, array(
             CURLOPT_URL => 'http://localhost:26659/submit_pfb',
@@ -16,7 +20,7 @@ if (isset($_POST)) {
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => '{"namespace_id": "0c204d39600fddd3", "data": "f1f20ca8007e910a3bf8b2e61da0f26bca07ef78717a6ea54165f5", "gas_limit": ' . __DEFAULT_GAS__ . ', "fee": ' . __DEFAULT_FEE__ . '}',
+            CURLOPT_POSTFIELDS => '{"namespace_id": "' . $namespace_id . '", "data": "' . $tx_data . '", "gas_limit": ' . __DEFAULT_GAS__ . ', "fee": ' . __DEFAULT_FEE__ . '}',
             CURLOPT_HTTPHEADER => array(
                 'Content-Type: application/x-www-form-urlencoded'
             ),
@@ -25,9 +29,26 @@ if (isset($_POST)) {
         $response = curl_exec($curl);
 
         curl_close($curl);
-        echo $response;
+        $tx_submit_data = json_decode($response);
+        $arr = [];
+        
+        $arr['txhash'] = $txhash;
+        $arr['namespace_id'] = $namespace_id;
+        $arr['data_hex'] = $tx_data;
+        $arr['data'] = $data['tx_data'];
+        $arr['gas_wanted'] = $gas_wanted;
+        $arr['gas_used'] = $gas_used;
+        $arr['height'] = $height;
+        $arr['code'] = 200;
+        $arr['date'] = date('Y-m-d H:i:s', time());
+
+        json_encode($arr, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+           
     } catch (\Throwable $th) {
         $err = '{"error": 500, "message": "' . $th->getMessage() . '"}';
         echo $err;
     }
+} else {
+    $err = '{"error": 500, "message": "Insufficient data"}';
+    echo $err;
 }
